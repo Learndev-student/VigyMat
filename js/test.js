6b9313d76489816bf4996e52233c69c919f0f3c1
root <dehariyasrajan@gmail.com>
2021-10-30 21:11:38 +0530

    1.0.0.0.1

--- js/test.js
+++ js/test.js
@@ -8,8 +8,4 @@ function test_input1(input2){
 function test_input2(input1){
 	return input1/5;
 }
-<<<<<<< HEAD
 export function functions=[test_input1,test_input2];
-=======
-export var func_test=[test_input1,test_input2];
->>>>>>> 7ea985a61143ca18c5b3616f7ad535fb5b8ac452
