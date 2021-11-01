7567e780778f99ac47fe1e3f582ad8109c72e782
root <dehariyasrajan@gmail.com>
2021-11-01 15:25:06 +0530

    1.0.0.0.2

--- js/test.js
+++ js/test.js
@@ -1,11 +1,12 @@
 export const obj = {
 	length:2,
-	placeholders:["input1","input2"]
+	placeholders:["input1","input2"],
+	type:["number","number"]
 };
-function test_input1(input2){
-	return input2*5;
+function test_input1(arr){
+	return arr[0]*5;
 }
-function test_input2(input1){
-	return input1/5;
+function test_input2(arr){
+	return arr[0]/5;
 }
 export const functions=[test_input1,test_input2];
