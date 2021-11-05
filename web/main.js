4b07a7d7a4af3133a5ba8693ded03a07aaa88975
root <dehariyasrajan@gmail.com>
2021-11-05 16:14:39 +0530

    1.0.0.0.7

--- web/main.js
+++ web/main.js
@@ -1,4 +1,5 @@
 import {get} from "https://learndev-student.github.io/VigyMat/web/modules.js";
+
 var url_arr=(window.location.href).split('/');
 switch (url_arr[4]){
 	case "modules": get(url_arr);
