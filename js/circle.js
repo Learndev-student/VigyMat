81b78f30d20067220021531a0e604caec860af3f
Learndev-pro <dehariyasrajan0149@gmail.com>
2021-12-10 16:28:45 +0530

    multiple io integration

--- js/circle.js
+++ js/circle.js
@@ -11,15 +11,15 @@ export const Obj={
 	headings:["About","Calculate","Algorithm"],
 	content:["This is again a testing module. But this time with circles, trying to get multiple output from single inpiut and more.",obj,"Here the problem is to give multiple outputs from a single input in a combination."]
 };
-function f1(arr){
+function f0(arr){
 	return [arr[0],arr[0]*math.pi*2,arr[0]*arr[0]*math.pi];
 }
-function f2(arr){
+function f1(arr){
 	return [math.sqrt(arr[1]/math.pi),math.sqrt(arr[1]/math.pi)*math.pi*2];
 }
-function f3(arr){
+function f2(arr){
 	let r=arr[2]/(math.pi*2);
 	return [r,math.pi*r*r,arr[2]];
 }
 export const list=[[1,0,0],[0,1,0],[0,0,1]];
-export const functions=[];
+export const functions=[f0,f1,f2];
