1e73a2c03617c8ae5b113ffa4bd7ab9487566f23
Learndev-pro <dehariyasrajan0149@gmail.com>
2021-12-16 15:45:58 +0530

    Code formatted

--- web/main.js
+++ web/main.js
@@ -1,63 +1,77 @@
 //Importing get() from modules.js to import additional modules - that is the dynamic pages scripts
-import {get} from "https://learndev-student.github.io/VigyMat/web/modules.js";
+import { get } from "https://learndev-student.github.io/VigyMat/web/modules.js";
 
 //Content renderer
-function render_content(obj){
-	let app=document.getElementById("app");
-	document.getElementsByTagName("title")[0].textContent=obj.title;
+function render_content(obj) {
+	let app = document.getElementById("app");
+	document.getElementsByTagName("title")[0].textContent = obj.title;
 	app.innerHTML="";
-	let n=0;
-	obj["headings"].forEach((i)=>{
-		let box=document.createElement("div");
+	let n = 0;
+	obj["headings"].forEach( i => {
+		let box = document.createElement("div");
 		box.className="box";
-		let heading=document.createElement("h2");
-		heading.className="heading";
-		heading.textContent=i;
+		let heading = document.createElement("h2");
+		heading.className = "heading";
+		heading.textContent = i;
 		box.appendChild(heading);
-		if(typeof obj["content"][n]=="string"){
-			box.innerHTML+=obj["content"][n++];
-		}else{
-			obj["content"][n++].forEach((e)=>box.appendChild(e));
+		if( typeof obj["content"][n] == "string" ) {
+			box.innerHTML += obj["content"][n++];
+		} else {
+			obj["content"][n++].forEach( e => box.appendChild(e) );
 		}
 		app.appendChild(box);
-	});
+	} );
 	d_links();
-	let l=document.getElementsByClassName("location")[0];
+	let l = document.getElementsByClassName("location")[0];
 	let x;
-	if(window.location.href.split('/')[5]==undefined){ x="";}else{x=window.location.href.split('/')[5];}
-	l.textContent=`${window.location.href.split('/')[4]}/${x}`;
+	if(window.location.href.split('/')[5] == undefined) {
+		x = "";
+	} else {
+		x = window.location.href.split('/')[5];
+	}
+	l.textContent = `${window.location.href.split('/')[4]}/${x}`;
 }
 
 //An async function to load the modules
-async function load_page(){
-var url_arr=(window.location.href).split('/');
-switch (url_arr[4]){
-	case "modules": 
-		get(url_arr).then((o)=>render_content(o));
-		break;
-	case "":
-		{let {Obj}=await import("https://learndev-student.github.io/VigyMat/web/main/home.js");
-		render_content(Obj);}
-		break;
-	default :
-		{let {Obj}= await import("https://learndev-student.github.io/VigyMat/web/main/404.js");
-		render_content(Obj);}
-};
+async function load_page() {
+	var url_arr=(window.location.href).split('/');
+	switch ( url_arr[4] ) {
+		case "modules" : 
+			get(url_arr).then( o => render_content(o) );
+			break;
+		case "" :
+			{
+				let { Obj } = await import("https://learndev-student.github.io/VigyMat/web/main/home.js");
+				render_content(Obj);
+			}
+			break;
+		default :
+			{
+				let { Obj } = await import("https://learndev-student.github.io/VigyMat/web/main/404.js");
+				render_content(Obj);
+			}
+	};
 }
 
 //Initialising so that the current required module can be loaded
 load_page();
-function d_links(){
-	let links=document.getElementsByClassName('d_link');
-	for(let n=0;n<links.length;n++){
-		links[n].onclick=(event)=>{
-			history.pushState({},'',links[n].href);
-			get((links[n].href).split('/')).then((o)=>render_content(o));
+
+//Dynamic link function for making dynamic links
+function d_links() {
+	let links = document.getElementsByClassName('d_link');
+	for( let n=0 ; n<links.length ; n++ ) {
+		links[n].onclick=( event )=> {
+			history.pushState({} , '' , links[n].href );
+			get( (links[n].href).split('/')).then( o =>render_content(o) );
 			event.preventDefault();
 		};
+	}
 }
-}
+
+//Initialising the dynamic links in current page
 d_links();
+
+//Event handler for change in the url of the page
 window.onpopstate=()=>{
 	load_page();
 };
--- web/modules.js
+++ web/modules.js
@@ -1,127 +1,158 @@
-const data={
-        "languages":1,
-        "js":{
-        	"length":3,
-		"titles":["Test Module","Circle","Cube"],
-		"urls":["/Misc/test","/Misc/circle","/Misc/cube"]
+//Const data for the list of modules
+const data = {
+        "languages" : 1,
+        "js" : {
+        	"length" : 3,
+		"titles" : [ 
+			"Test Module" ,
+			"Circle" ,
+			"Cube" 
+		] ,
+		"urls" : [ 
+			"/Misc/test" ,
+			"/Misc/circle" ,
+			"/Misc/cube" 
+		]
         }
 };
-const modules=data['js'];
+
+//Selecting js modules for web pages
+const modules = data['js'];
+//A container for module functins
 var functions;
-var inp_arr=[];
+//A container for input/output elements
+var inp_arr = [];
+//A container for checking the correct order of input and output elements
 var list;
-function arraysEqual(a, b) {
-	  if (a === b) return true;
-	  if (a == null || b == null) return false;
-	  if (a.length !== b.length) return false;
-	  for (var i = 0; i < a.length; ++i) {
-             if (a[i] !== b[i]) return false;
+
+//Checks if twomarrays are equal or not
+function arraysEqual( a, b ) {
+	  if ( a === b ) return true;
+	  if ( a == null || b == null ) return false;
+	  if ( a.length !== b.length ) return false;
+	  for ( var i = 0 ; i < a.length ; ++i ) {
+             if ( a[i] !== b[i] ) return false;
 	  }         return true;
 }
+
+//The calculate function to initialise the module functions according to the order of input and output elements
 function calculate(){
-	let a=[];
-	let ans=[];
-	inp_arr.forEach((i)=>{
-		if(i.dataset.io=="input"){
+	let a = [];
+	let ans = [];
+	inp_arr.forEach( i => {
+		if( i.dataset.io == "input" ) {
 			a.push(1);
-		}else{
+		} else {
 			a.push(0);
 		}
 	});
-	for(let n=0;n<list.length;n++){
-		if(arraysEqual(a,list[n])){
-			ans=functions[n](inp_arr);
-			let m=0;
-			inp_arr.forEach((i)=>{
-				if(m!=n) i.value=ans[m];
+	for( let n=0 ; n<list.length ; n++ ) {
+		if( arraysEqual(a,list[n]) ) {
+			ans = functions[n](inp_arr);
+			let m = 0;
+			inp_arr.forEach( i => {
+				if( m != n ) i.value = ans[m];
 				m++;
 			});
 			break;
 		}
 	}
 }
-function reset(Obj){
-	let obj=Obj.content[Obj.calc];
-	let container= document.createElement("div");
+
+//Reseting the page's content
+function reset( Obj ) {
+	let obj = Obj.content[ Obj.calc ];
+	let container = document.createElement("div");
 	container.innerHTML="";
 	let i;
-	let ele_arr=[];
+	let ele_arr = [];
 	console.log(obj);
-	if(Obj.loaded){
+	if( Obj.loaded ) {
 		return Obj;
-	}else{
-		for(i=0;i<obj.placeholders.length;i++){
-			let input=document.createElement("input");
-			inp_arr[i]=input;
-			input.placeholder=obj.placeholders[i];
-			input.type=obj.type[i];
-			input.name=i;
-			input.className="inputs";
-			input.dataset.io="output";
-			input.onclick=(i)=>{
-				i.srcElement.dataset.io="input";
-			}
-			input.oninput=calculate;
-			input.onchange=calculate;
+	} else {
+		for( i=0 ; i<obj.placeholders.length ; i++ ) {
+			let input = document.createElement("input");
+			inp_arr[i] = input;
+			input.placeholder = obj.placeholders[i];
+			input.type = obj.type[i];
+			input.name = i;
+			input.className = "inputs";
+			input.dataset.io = "output";
+			input.onclick = (i)=> {
+				i.srcElement.dataset.io = "input";
+			};
+			input.oninput = calculate;
+			input.onchange = calculate;
 			ele_arr.push(input);
 		}
-		let clear=document.createElement("input");
-		clear.type="button";
-		clear.value="Clear";
-		clear.id="clear";
-		clear.onclick=()=>{
-			inp_arr.forEach((i)=>{
-				i.value="";
-				i.dataset.io="output";
+		let clear = document.createElement("input");
+		clear.type = "button";
+		clear.value = "Clear";
+		clear.id = "clear";
+		clear.onclick = ()=> {
+			inp_arr.forEach( i => {
+				i.value = "";
+				i.dataset.io = "output";
 			});
 		};
 		ele_arr.push(clear);
-		Obj.content[Obj.calc]=ele_arr;
-		Obj.loaded=true;
+		Obj.content[Obj.calc] = ele_arr;
+		Obj.loaded = true;
 		return Obj;
 	}
 }
-async function load_module(str){
+
+//The main function which imports modules dynamically
+async function load_module( str ) {
 	let Obj;
-	inp_arr=[];
-        functions=[];
-	let module= await import(`https://learndev-student.github.io/VigyMat/web/js${str}.js`);
-	Obj=module.Obj;
-	list=module.list;
-	functions=module.functions;
-	Obj=reset(Obj);
+	inp_arr = [];
+        functions = [];
+	let module = await import(`https://learndev-student.github.io/VigyMat/web/js${str}.js`);
+	Obj = module.Obj;
+	list = module.list;
+	functions = module.functions;
+	Obj = reset(Obj);
 	console.log(`${str} module loaded`);
 	return Obj;
 }
-function module_not_found(){
-	let url_arr=window.location.href.split('/');
-	let str=`${url_arr[4]}/${url_arr[5]}`;
-	let Obj={
-		description:"404",
-		headings:["404!"],
-		content:[`<h3>Sorry! ,</h3><p>The page ${str} not found. You may check the <a class="d_link" href="https://learndev-student.github.io/VigyMat">Homepage</a>`]
+
+//What if the module is not present? Here the thing to do
+function module_not_found() {
+	let url_arr = window.location.href.split('/');
+	let str = `${url_arr[4]}/${url_arr[5]}`;
+	let Obj = { 
+		description:"404" ,
+		headings:[
+			"404!"
+		] ,
+		content:[
+			`<h3>Sorry! ,</h3><p>The page ${str} not found. You may check the <a class="d_link" href="https://learndev-student.github.io/VigyMat">Homepage</a>`
+		]
 	};
 	return Obj;
 }
-async function get(arr){
+
+//Get the link of the module which has to be imported dynamically
+async function get( arr ) {
 	let n;
 	let Obj;
-	let a="";
-	arr.forEach( i =>{
-		if(arr.indexOf(i)>=5) a+=`/`+i;
+	let a = "";
+	arr.forEach( i => {
+		if( arr.indexOf(i) >= 5 ) a += `/`+i;
 		console.log(a);
 	});
 	console.log(modules.urls);
-	for(n=0;n<modules.length;n++){
-		if(modules.urls[n]==a){
-			Obj=await load_module(a);
+	for( n=0 ; n<modules.length ; n++ ) {
+		if( modules.urls[n] == a ){
+			Obj = await load_module(a);
 			break;
 		}
 	}
-	if(n==modules.length){
-		Obj=module_not_found();
+	if( n == modules.length ) {
+		Obj = module_not_found();
 	}
 	return Obj;
 }
 
-export {get};
+//Export get() so that others can use it
+export { get };
