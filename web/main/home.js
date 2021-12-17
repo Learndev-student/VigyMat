13813707fcc4c069aec8b57440a374cdd3816ea0
Learndev-pro <dehariyasrajan0149@gmail.com>
2021-12-17 15:26:36 +0530

    Home

--- web/main/home.js
+++ web/main/home.js
@@ -3,10 +3,15 @@ export const Obj = {
 	description : "Home Page" ,
 	headings : [
 		"About" ,
-		"Why?"
+		"MODULES"
 	] ,
 	content : [
-		"<p>The \"Open-source project\" <b>VigyMat</b> , is just an initiative to just make the world of complex scientific and mathematical calculations easy to you.</p>" ,
-		"<p>It has started just with my hobby of coding. Well, it has not yet completed its base structure</p>"
+		"<p>The \"Open-source project\" <b>VigyMat</b> , is just an initiative to just make the world of complex scientific and mathematical calculations easy to you.</p><p>How? well this has started all with the simple webpage of VigyMat and used the open-sourcing of github to provide and develop this in many languages.(Atleast is is what our aim is!)" ,
+		`<p>Until now there are only 3 modules available<p/>
+		<ul class='module_list'>
+			<li><a href="/VigyMat/modules/Misc/test.js">Test</a></li>
+			<li><a href="/VigyMat/modules/Misc/cube.js">Cube</a></li>
+			<li><a href="/VigyMat/modules/Misc/sphere.js">Sphere</a></li>
+		</ul>`
 	]
 };
