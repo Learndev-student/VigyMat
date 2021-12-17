3871ebb883f0609a0851560fb285ed953702ba37
Learndev-pro <dehariyasrajan0149@gmail.com>
2021-12-17 16:19:31 +0530

    home

--- data.js
+++ data.js
@@ -2,15 +2,24 @@ export const data = {
 	"languages" : 1,
         "js" : {
         	"length" : 3,
-		"titles" : [ 
-			"Test Module" ,
-			"Sphere" ,
-			"Cube" 
-		] ,
-		"urls" : [ 
-			"/Misc/test" ,
-			"/Misc/sphere" ,
-			"/Misc/cube" 
+		"sub" : [
+			"Misc" ,
+			"Geometry" ,
+			"Trigonometry"
+		],
+		"content" : [ 
+			{
+				"Misc" : [
+					"Test" 
+				] ,
+				"length" : 1
+			} ,
+			{
+				"Geometry" : [ 
+					"Sphere" ,
+					"Cube" 
+				] ,
+				"length" : 2
+			}
 		]
-        }
 };
--- web/main/home.js
+++ web/main/home.js
@@ -1,17 +1,24 @@
+import { data } from "/VigyMat/data.js";
+let modules = data['js'];
+let sub = modules.sub; 
+let a;
+for ( let n=0 ; n < sub.length ; n++){
+	a += `<p>${sub[n]}</p><ul class='d_ul'>`;
+	let titles = modules.content[n][sub[n]];
+	titles.forEach( i =>{
+		a += `<li><a class="d_link" href='/VigyMat/modules/${sub[n]}/${i}'>${i}</a></li>`;
+	});
+	a += `</ul>`;
+}
 export const Obj = {
 	title : "Home" ,
 	description : "Home Page" ,
 	headings : [
 		"About" ,
-		"MODULES"
+		"Modules"
 	] ,
 	content : [
 		"<p>The \"Open-source project\" <b>VigyMat</b> , is just an initiative to just make the world of complex scientific and mathematical calculations easy to you.</p><p>How? well this has started all with the simple webpage of VigyMat and used the open-sourcing of github to provide and develop this in many languages.(Atleast is is what our aim is!)" ,
-		`<p>Until now there are only 3 modules available<p/>
-		<ul class='module_list'>
-			<li><a href="/VigyMat/modules/Misc/test">Test</a></li>
-			<li><a href="/VigyMat/modules/Misc/cube">Cube</a></li>
-			<li><a href="/VigyMat/modules/Misc/sphere">Sphere</a></li>
-		</ul>`
+		a
 	]
 };
